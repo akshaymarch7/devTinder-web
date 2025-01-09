4781866531673edd28433bd8743b7111482ac139
Akshay Saini <akshaymarch7@gmail.com>
2025-01-09 21:29:48 +0530

    update premium verifcayion on page load

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Premium = () => {
   const [isUserPremium, setIsUserPremium] = useState(false);
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
 
   const verifyPremiumUser = async () => {
     const res = await axios.get(BASE_URL + "/premium/verify", {
